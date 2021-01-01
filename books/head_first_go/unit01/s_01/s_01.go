65f2e70bdd053f372edc1f7893d387b8dee28f48
Shin <cu7898@gmail.com>
2021-01-01 23:32:10 +0900

    Study 반환값의 이해
    Book Head First Go

---
+++ books/head_first_go/unit01/s_01/s_01.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	// 반환값을 확인 할 수 있는 fmt.Println 함수
+	fmt.Println(math.Floor(2.74))
+	fmt.Println(strings.Title("head first go"))
+}
