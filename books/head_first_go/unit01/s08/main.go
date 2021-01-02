95398f847a2d04f81660313d34ca84e6daa2179a
Shin <cu7898@gmail.com>
2021-01-02 10:58:37 +0900

    Study 단축 변수
    Book Head First Go

---
+++ books/head_first_go/unit01/s08/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// 단축 변수 선언
+func main() {
+	quantity := 4
+	length, width := 1.2, 2.4
+	customerName := "Damon Cole"
+
+	fmt.Println(customerName)
+	fmt.Println("has ordered", quantity, "sheets")
+	fmt.Println("each with an area of")
+	fmt.Println(length*width, "square meters")
+}
