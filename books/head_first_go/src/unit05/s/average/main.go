966ec922f87d41fa3aca031529da1510f991e7c6
shin <cu7898@gmail.com>
2021-01-07 08:46:04 +0900

    Study unit05 배열 완료
    Book: 헤드 퍼스트 Go

---
+++ books/head_first_go/src/unit05/s/average/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"unit05/s/datafile"
+)
+
+func main() {
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sum := 0.0
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+}
