5a83dd77a30855b2f62462153d9636915df8daba
Shin <cu7898@gmail.com>
2021-01-23 00:37:43 +0900

    2020-01-23

---
+++ books/head_first_go/src/unit11/s/02/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"unit11/s/02/mypkg"
+)
+
+func main() {
+	var value mypkg.MyInterface
+	value = mypkg.MyType(5)
+	value.MethodWithoutParameters()
+	value.MethodWithParameter(127.3)
+}
